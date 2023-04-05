efabaf669611baa9f02c7d9081bcb540b225492b
Thomas DA ROCHA <8969556+taorepoara@users.noreply.github.com>
2023-04-05 10:02:51 +0200

    feat: Upgrade template (#20)
    * feat: Upgrade template
    
    * fix: Navigation

--- views/app.js
+++ views/app.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { Text } = require('@lenra/components');
 const ui = require('./utils/ui.js')
 
 /**
@@ -9,12 +10,7 @@ const ui = require('./utils/ui.js')
  */
 module.exports = (navigations, _props) => {
   const navigation = navigations[0];
-  if (!navigation) {
-    return {
-      type: "text",
-      value: "Loading"
-    }
-  }
+  if (!navigation) return Text("Loading");
   let app = {
     type: "flex",
     direction: "vertical",
